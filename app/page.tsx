286c240aaa0b621fe77ef91623c517b7507caa98
freshtildeath009 <visrobis@gmail.com>
2025-01-10 22:28:10 +0800

    fix some issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <Link href={"/dashboard/samara-beach/samara-booking-list"}>List </Link>
+      {/* <Link href={"/dashboard/samara-beach/samara-booking-list"}>List </Link> */}
     </div>
   );
 }
